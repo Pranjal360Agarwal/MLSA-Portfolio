232c9186f578e0a68fe62571ed12560311c8a48d
Pranjal Agarwal <98935622+Pranjal360Agarwal@users.noreply.github.com>
2023-10-01 12:50:11 +0000

    home page changed

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Pranjal Agarwal",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "pranjal360agarwal@gmail.com",
+  gitHub: "Pranjal360Agarwal",
+  instagram: "pranjaltechsaver",
+  linkedIn: "pranjalagarwal99",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "pranjal",
+  youTube: "pranjal",
 };
 
 const primaryColor = "#4E567E";
